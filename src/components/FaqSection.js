75da6015f228dca01b8716c474947579e53dac0d
Mads Akselsen <akselsenmads@gmail.com>
2021-04-04 16:02:08 +0200

    added nav

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,38 +1,86 @@
 import React from 'react';
+import styled from 'styled-components';
+import {
+  StyledLayout,
+  StyledDescription,
+  StyledImageContainer,
+} from '../styles';
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <StyledFaq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
       <div className="question">
         <h4>How Do I Start?</h4>
-        <div className="answer"></div>
-        <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
-        <div className="answer"></div>
-        <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>How Do I Start</h4>
-        <div className="answer"></div>
-        <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Methods</h4>
-        <div className="answer"></div>
-        <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Products do you offer?</h4>
-        <div className="answer"></div>
-        <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Culpa deserunt sit exercitation ipsum est magna dolor et sunt.</p>
+        </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </StyledFaq>
   );
 };
+
+const StyledFaq = styled(StyledLayout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
