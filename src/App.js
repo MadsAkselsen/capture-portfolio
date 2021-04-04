87ac69f902ef5a71c4069a814de1518c6398b1a1
Mads Akselsen <akselsenmads@gmail.com>
2021-04-04 20:31:51 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 // pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+// router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact component={AboutUs} />
+        <Route path="/work" component={OurWork} />
+        <Route path="/contact" component={ContactUs} />
+      </Switch>
     </div>
   );
 }
