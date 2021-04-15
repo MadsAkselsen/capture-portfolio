5976fb0e0e2ba5c4d688112ceafad3a4669ec5d8
Mads Akselsen <akselsenmads@gmail.com>
2021-04-15 18:59:41 +0200

    added test animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-
 import {
   StyledLayout,
   StyledDescription,
@@ -8,23 +7,47 @@ import {
   StyledHide,
 } from '../styles';
 
+import { motion } from 'framer-motion';
+
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeOut',
+        staggerChildren: 0.4,
+        when: 'beforeChildren',
+      },
+    },
+  };
+
   return (
     <StyledLayout>
       <StyledDescription className="description">
-        <div className="title">
+        <motion.div
+          className="title"
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
           <StyledHide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>true</h2>
+            <motion.h2 variants={titleAnim}>true</motion.h2>
           </StyledHide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videoagraphy ideas that you have. We
           have professionals with amazing skills
